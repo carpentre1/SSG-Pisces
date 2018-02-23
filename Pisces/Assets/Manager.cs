d02786a760038b3a5fbfbe4288a04a8d76dabd1d
carpentre1 <jlowell.carpenter@gmail.com>
2018-02-22 18:54:40 -0600

    Initial commit

---
+++ Pisces/Assets/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    //[HideInInspector]
+    public int numStudents = 0;
+    public int lakeFish;
+
+    //public Zodiac zodiacscript;
+
+    public GameObject Zodiacs;
+    public GameObject ZodiacPanels;
+    public GameObject Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces;
+
+    void BeginGame()
+    {
+        foreach (Transform child in Zodiacs.transform)
+        {
+            //child.GetComponent<Zodiac>().CheckStudents();
+        }
+    }
+
+    void FindNextTurn()
+    {
+        //find gameobject whose zodiac = turn number, use their TakeTurn(), then check if FinalTurnComplete()
+    }
+
+    void FinalTurnComplete()
+    {
+        //calculate new number of fish in lake, new number of fish in ponds
+    }
+}
---
+++ Pisces/Assets/Zodiac.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zodiac : MonoBehaviour {
+
+    //[HideInInspector]
+    public int pondFish;
+    public int numStudentsInZodiac = 0;
+
+    enum ZodiacType { Aries=1, Taurus=2, Gemini=3, Cancer=4, Leo=5, Virgo=6, Libra=7, Scorpio=8, Sagittarius=9, Capricorn=10, Aquarius=11, Pisces=12}
+
+    // Use this for initialization
+    void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CheckStudents()
+    {
+
+    }
+
+    public void AddStudents(string students)
+    {
+        numStudentsInZodiac = int.Parse(students);
+    }
+}
