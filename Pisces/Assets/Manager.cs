d880507ef05ba56f2c1b8665cc9c2dbc61a6fb14
carpentre1 <jlowell.carpenter@gmail.com>
2018-02-22 20:29:17 -0600

    Start done, working on turns
    Read input fields in manager, then send those values to zodiac while it waits?

--- Pisces/Assets/Manager.cs
+++ Pisces/Assets/Manager.cs
@@ -1,29 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour {
 
-    //[HideInInspector]
+    //variables
     public int numStudents = 0;
     public int lakeFish;
+    public int currentTurn = 0;
 
-    //public Zodiac zodiacscript;
-
+    //all the elements that hook into game manager
     public GameObject Zodiacs;
     public GameObject ZodiacPanels;
+    public GameObject StartButton;
     public GameObject Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces;
 
-    void BeginGame()
+    public GameObject TurnPanel;
+    public GameObject EatInputField;
+    public GameObject InvestInputField;
+    public GameObject ExpandInputField;
+    public GameObject TurnNameText;
+
+    //
+
+
+    public void BeginGame()
     {
-        foreach (Transform child in Zodiacs.transform)
+        numStudents = 0;//reset the number of students before starting a new game
+        foreach (Transform child in Zodiacs.transform)//add up all the students in each group
         {
-            //child.GetComponent<Zodiac>().CheckStudents();
+            child.GetComponent<Zodiac>().CheckStudents();
+            numStudents += child.GetComponent<Zodiac>().numStudentsInZodiac;
         }
+        Debug.Log(numStudents + " students total.");
+        ToggleStartInterface(false);
+        FindNextTurn();
+    }
+
+    private void ToggleStartInterface(bool active)
+    {
+        StartButton.SetActive(active);
+        ZodiacPanels.SetActive(active);
+
     }
 
     void FindNextTurn()
     {
+        while (currentTurn < 13)
+        {
+            currentTurn++;
+            foreach (Transform child in Zodiacs.transform)
+            {
+                if (child.GetComponent<Zodiac>().turnOrder == currentTurn)
+                {
+                    child.GetComponent<Zodiac>().TakeTurn();
+                }
+            }
+        }
+        Debug.Log("all turns taken");
         //find gameobject whose zodiac = turn number, use their TakeTurn(), then check if FinalTurnComplete()
     }
 
--- Pisces/Assets/Zodiac.cs
+++ Pisces/Assets/Zodiac.cs
@@ -4,29 +4,58 @@ using UnityEngine;
 
 public class Zodiac : MonoBehaviour {
 
-    //[HideInInspector]
-    public int pondFish;
+    //variables
+    public int fishInPond;
+    public int sizeOfPond;
     public int numStudentsInZodiac = 0;
 
+    bool turnCompleted = false;
+
+    //per-turn variables
+    int fishToCatch;
+    int fishToEat;
+    int fishToExpand;
+    int fishToInvest;
+
+    const int FISH_FOR_SURVIVAL = 4;
+
     enum ZodiacType { Aries=1, Taurus=2, Gemini=3, Cancer=4, Leo=5, Virgo=6, Libra=7, Scorpio=8, Sagittarius=9, Capricorn=10, Aquarius=11, Pisces=12}
+    public int turnOrder = 0;
 
     // Use this for initialization
     void Start() {
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void CheckStudents()
+    public void TakeTurn()
     {
-
+        while(!turnCompleted)
+        {
+            //wait for turn to complete
+        }
+        Debug.Log(this.name + " took their turn.");
     }
 
-    public void AddStudents(string students)
+    public void CheckStudents()//before starting the game, check the amount of students applied to each zodiac group
     {
+        if(numStudentsInZodiac == 0)//if no number was assigned at game start
+        {
+            numStudentsInZodiac = 2;//just set it to a default value of 2
+        }
+    }
+
+    public void AddStudents(string students)//edited by the start screen's input fields for each zodiac group
+    {
+        if (students == null || students == string.Empty)
+        {
+            numStudentsInZodiac = 0;
+            return;
+        }
         numStudentsInZodiac = int.Parse(students);
     }
 }
