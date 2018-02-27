91ec8fc11774855790c29fb60369de212b1f4a8e
carpentre1 <jlowell.carpenter@gmail.com>
2018-02-26 23:38:39 -0600

    Turn actions
    actions taken during turn are implemented, no UI updates yet. need to clear input fields too

--- Pisces/Assets/Manager.cs
+++ Pisces/Assets/Manager.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class Manager : MonoBehaviour {
 
     //variables
-    public int numStudents = 0;
-    public int lakeFish;
+    public float numStudents = 0;
+    public int lakeFish = 350;
     public int currentTurn = 0;
+    public int maxCatchableFish;
+
+    //variables that are logged during each turn
+    public int m_fishToEat = 0;
+    public int m_fishToExpand = 0;
+    public int m_fishToInvest = 0;
 
     //all the elements that hook into game manager
     public GameObject Zodiacs;
@@ -21,6 +27,8 @@ public class Manager : MonoBehaviour {
     public GameObject InvestInputField;
     public GameObject ExpandInputField;
     public GameObject TurnNameText;
+
+    public InputFields eatField;
 
     //
 
@@ -35,6 +43,11 @@ public class Manager : MonoBehaviour {
         }
         Debug.Log(numStudents + " students total.");
         ToggleStartInterface(false);
+
+        //divide students by 1.6(?) and round down to find how many fish each group can catch
+        maxCatchableFish = (int)Mathf.Floor(numStudents*1.6f);//change this magic number to the real formula's value later
+
+        ToggleTurnInterface(true);
         FindNextTurn();
     }
 
@@ -45,9 +58,15 @@ public class Manager : MonoBehaviour {
 
     }
 
-    void FindNextTurn()
+    private void ToggleTurnInterface(bool active)
     {
-        while (currentTurn < 13)
+        TurnPanel.SetActive(active);
+
+    }
+
+    public void FindNextTurn()
+    {
+        if (currentTurn < 13)
         {
             currentTurn++;
             foreach (Transform child in Zodiacs.transform)
@@ -58,8 +77,55 @@ public class Manager : MonoBehaviour {
                 }
             }
         }
-        Debug.Log("all turns taken");
+        else
+        {
+            Debug.Log("all turns taken");
+        }
         //find gameobject whose zodiac = turn number, use their TakeTurn(), then check if FinalTurnComplete()
+    }
+
+    public void SubmitButtonClicked()
+    {
+        //clear the input fields
+
+        foreach (Transform child in Zodiacs.transform)
+        {
+            if (child.GetComponent<Zodiac>().turnOrder == currentTurn)
+            {
+                child.GetComponent<Zodiac>().TurnSubmission(m_fishToEat, m_fishToInvest, m_fishToExpand);
+                break;
+            }
+        }
+    }
+
+    public void LogEat(string fish)//reads the input field during turns so that the proper zodiac group can retrieve the variable
+    {
+        if (fish == null || fish == string.Empty)
+        {
+            m_fishToEat = 0;
+            return;
+        }
+        m_fishToEat = int.Parse(fish);
+    }
+
+    public void LogExpand(string fish)
+    {
+        if (fish == null || fish == string.Empty)
+        {
+            m_fishToExpand = 0;
+            return;
+        }
+        m_fishToExpand = int.Parse(fish);
+    }
+
+    public void LogInvest(string fish)
+    {
+        if (fish == null || fish == string.Empty)
+        {
+            m_fishToInvest = 0;
+            return;
+        }
+        m_fishToInvest = int.Parse(fish);
     }
 
     void FinalTurnComplete()
--- Pisces/Assets/Zodiac.cs
+++ Pisces/Assets/Zodiac.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,10 @@ public class Zodiac : MonoBehaviour {
     public int numStudentsInZodiac = 0;
 
     bool turnCompleted = false;
+    bool itIsMyTurn = false;
+
+    public GameObject gameManager;
+    Manager manager;
 
     //per-turn variables
     int fishToCatch;
@@ -24,7 +29,7 @@ public class Zodiac : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
+        manager = gameManager.GetComponent<Manager>();
     }
 	
 	// Update is called once per frame
@@ -32,13 +37,86 @@ public class Zodiac : MonoBehaviour {
 		
 	}
 
-    public void TakeTurn()
+    public void TakeTurn()//called when the manager decides it's their turn
     {
-        while(!turnCompleted)
+        itIsMyTurn = true;
+        Debug.Log("It is " + this.name + "'s turn.");
+    }
+
+    public void TurnSubmission(int eat, int invest, int expand)//when they click the submit button during their turn
+    {
+        if(!itIsMyTurn)//if it's not their turn, don't let them submit
         {
-            //wait for turn to complete
+            Debug.Log(this.name + " tried to submit when it wasn't their turn");
+            return;
         }
-        Debug.Log(this.name + " took their turn.");
+        fishToEat = eat; fishToInvest = invest; fishToExpand = expand;//assign the input field values to this group
+
+        fishToCatch = fishToEat + fishToExpand + fishToInvest;//catch the sum of desired fish to eat/expand/invest
+        CheckFishQuantity();
+        Debug.Log("caught:" + fishToCatch);
+        PrioritizeFish();
+        AnnounceTurnResults();
+        RecordResults();
+        itIsMyTurn = false;
+        manager.FindNextTurn();
+    }
+
+    private void RecordResults()
+    {
+        //TODO:
+        //check to see if anyone dies
+        //add bonus eaten fish to score
+        //expand pond size
+        //increase invested fish size
+
+        manager.lakeFish -= (fishToEat + fishToCatch + fishToExpand);//remove caught fish from lake
+
+        //erase the values now that we're done
+        manager.m_fishToEat = 0; manager.m_fishToExpand = 0; manager.m_fishToInvest = 0;
+        fishToEat = 0; fishToExpand = 0; fishToInvest = 0;
+        fishToCatch = 0;
+    }
+
+    private void AnnounceTurnResults()
+    {
+        Debug.Log(this.name + " ate " + fishToEat + " fish.");
+        Debug.Log(this.name + " expanded using " + fishToExpand + " fish.");
+        Debug.Log(this.name + " invested " + fishToInvest + " fish.");
+    }
+
+    void CheckFishQuantity()//limits how many fish they can catch
+    {
+        if (manager.lakeFish < fishToCatch)
+        {
+            fishToCatch = manager.lakeFish;
+        }
+        if (fishToCatch > manager.maxCatchableFish)
+        {
+            fishToCatch = manager.maxCatchableFish;
+        }
+        Debug.Log(this.name + " caught " + fishToCatch);
+    }
+    void PrioritizeFish()//if they tried to eat/invest/expand more than they're allowed to catch, limit the values in order of eat>expand>invest
+    {
+        if(fishToCatch <= fishToEat)//if they don't have enough to eat the desired amount, eat everything that they could catch
+        {
+            fishToEat = fishToCatch;
+        }
+        fishToCatch -= fishToEat;//remove the fish eaten from the amount you have
+        if (fishToCatch < fishToExpand)//if they have enough to eat, but lacking after...
+        {
+            fishToExpand = fishToCatch;
+        }
+        fishToCatch -= fishToExpand;
+        if (fishToCatch < fishToInvest)//if they ate and expanded, but don't have enough to invest...
+        {
+            fishToInvest = fishToCatch;
+        }
+        else
+        {
+            Debug.Log("all went as planned");//remove this debug message later
+        }
     }
 
     public void CheckStudents()//before starting the game, check the amount of students applied to each zodiac group
